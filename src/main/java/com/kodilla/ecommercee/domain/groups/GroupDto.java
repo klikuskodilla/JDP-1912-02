6699663dffb591d2f6760c219c6a6a1a4da8821d
MSz <marcin.szymanski.poczta@gmail.com>
2019-12-22 17:12:38 +0100

    initial commit

---
+++ src/main/java/com/kodilla/ecommercee/domain/groups/GroupDto.java
@@ -0,0 +1,20 @@
+package com.kodilla.ecommercee.domain.groups;
+
+import com.kodilla.ecommercee.domain.products.ProductDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class GroupDto {
+    private Long groupId;
+    private String groupName;
+    private List<ProductDto> productsGroup = new ArrayList<>();
+}
