c303cdfc0ed53e9e0b053dc3918dc3dc4f072723
MSz <marcin.szymanski.poczta@gmail.com>
2019-12-23 17:17:40 +0100

    dummy implementation - update

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -4,6 +4,7 @@ import com.kodilla.ecommercee.domain.groups.GroupDto;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/")
