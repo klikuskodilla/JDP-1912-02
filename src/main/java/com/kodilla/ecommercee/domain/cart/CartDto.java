b8d5e11589079f95fa53f1dfcc47173d9af7327f
Nikoguj <Nikoguj@gmail.com>
2019-12-23 17:16:13 +0100

    JDP191202-14

---
+++ src/main/java/com/kodilla/ecommercee/domain/cart/CartDto.java
@@ -0,0 +1,20 @@
+package com.kodilla.ecommercee.domain.cart;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartDto {
+    private Long id;
+    private BigDecimal cost;
+    private List<ProductEntity> products;
+    private UserEntity user;
+    private OrderEntity order;
+
+}
