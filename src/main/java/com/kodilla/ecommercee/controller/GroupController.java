cfcd4e57f7843ab0b18d488b9b68df75cef1ee0e
MSz <marcin.szymanski.poczta@gmail.com>
2019-12-24 13:55:41 +0100

    GroupController - dummy tests

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -6,21 +6,28 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+
 @RestController
 @RequestMapping("/v1/")
 public class GroupController {
 
-   public List<GroupDto> getGroups() {
+    @RequestMapping(method = RequestMethod.GET, value = "getGroups")
+    public List<GroupDto> getGroups() {
         return new ArrayList<>();
     }
 
-    public void createGroup(GroupDto groupDto) {
+    @RequestMapping(method = RequestMethod.POST, value = "createGroup", consumes = APPLICATION_JSON_VALUE)
+    public void createGroup(@RequestBody GroupDto groupDto) {
     }
-    public GroupDto getGroup(Long groupId) {
-        return new GroupDto(1L,"nazwa grupy","grupa produktow");
+    @RequestMapping(method = RequestMethod.GET, value = "{groupId}")
+    public GroupDto getGroup(@PathVariable Long groupId) {
+        return new GroupDto(1L,"name group");
     }
 
-    public GroupDto updateGroup(GroupDto groupDto) {
-        return new GroupDto(1L,"nowa grupa","nowe produkty");
+    @RequestMapping(method = RequestMethod.PUT, value = "updateGroup")
+    public GroupDto updateGroup(@RequestBody GroupDto groupDto) {
+        return new GroupDto(1L,"update group");
     }
 }
