bc75f9c197575c5ce87db38d27f546f636cf2b95
MSz <marcin.szymanski.poczta@gmail.com>
2019-12-24 11:19:22 +0100

    update - preparing for tests (disabled entity relation)

--- src/main/java/com/kodilla/ecommercee/domain/groups/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/groups/Group.java
@@ -1,13 +1,12 @@
 package com.kodilla.ecommercee.domain.groups;
 
 //import com.kodilla.ecommercee.domain.Product.Product;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @NoArgsConstructor
 @Getter
