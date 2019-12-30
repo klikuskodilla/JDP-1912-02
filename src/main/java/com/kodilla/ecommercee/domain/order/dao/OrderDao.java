8b96230bce9d447977ac038160392e7bdc7102bf
naros1 <sorankasim@gmail.com>
2019-12-30 10:31:58 +0100

    @Transacional import corrected to Spring one

--- src/main/java/com/kodilla/ecommercee/domain/order/dao/OrderDao.java
+++ src/main/java/com/kodilla/ecommercee/domain/order/dao/OrderDao.java
@@ -3,8 +3,8 @@ package com.kodilla.ecommercee.domain.order.dao;
 import com.kodilla.ecommercee.domain.order.OrderEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 
 @Transactional
 @Repository
