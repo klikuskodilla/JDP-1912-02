3abd9085a743447edc864754aeaea03c142898e7
KrzysztofZakrocki <zakrocki.krzysztof@gmail.com>
2019-12-31 15:29:46 +0100

    correction before commit

--- src/main/java/com/kodilla/ecommercee/domain/Product/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product/Product.java
@@ -1,47 +1,79 @@
 package com.kodilla.ecommercee.domain.Product;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
 @Entity
 @Table(name = "PRODUCT")
 public class Product{
+
+    private Long id;
+    private String description;
+    private BigDecimal price;
+   // private Group group;
+   // private List<CartEntity> carts;
+
+    public Product() {
+    }
+
+    public Product(String description, BigDecimal price/*, Group group*/) {
+        this.description = description;
+        this.price = price;
+       // this.group = group;
+    }
+
 
     @Id
     @GeneratedValue
     @NotNull
     @Column(name = "PRODUCT_ID", unique=true)
-    private Long id;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     @Column(name = "DESCRIPTION")
-    private String description;
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     @NotNull
     @Column(name = "PRICE")
-    private BigDecimal price;
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 
  /*   @ManyToOne
-    @JoinColumn(name="GROUP_ID")
+    @JoinColumn(name = "GROUP_ID")
     @Column(name = "GROUP")
-    private Group group;
+    public Group getGroup() {
+        return group;
+    }
 
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(
-            name = "JOIN_PRODUCTS_CARTS",
-            joinColumns = {@JoinColumn (name = "PRODUCT_ID", referencedColumnName = "PRODUCT_ID")},
-            inverseJoinColumns = {@JoinColumn (name = "CART_ID", referencedColumnName = "CART_ID")}
-            )
-    List<CartEntity> carts;
-*/
+    public void setGroup(Group group) {
+        this.group = group;
+    }
 
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "products")
+    public List<CartEntity> getCarts() {
+        return carts;
+    }
+
+    public void setCarts(List<CartEntity> carts) {
+        this.carts = carts;
+    }
+
+  */
 }
