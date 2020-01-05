c16d104340ea94446355d3602c3a49558ffabda3
naros1 <sorankasim@gmail.com>
2020-01-05 17:05:24 +0100

    changes requested by klikuskodilla applied

--- src/test/java/com/kodilla/ecommercee/domain/user/dao/UserDaoTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/user/dao/UserDaoTestSuite.java
@@ -1,10 +1,5 @@
 package com.kodilla.ecommercee.domain.user.dao;
-
-import com.kodilla.ecommercee.domain.Product.Product;
-import com.kodilla.ecommercee.domain.Product.dao.ProductDao;
 import com.kodilla.ecommercee.domain.cart.CartEntity;
-import com.kodilla.ecommercee.domain.cart.dao.CartEntityDao;
-import com.kodilla.ecommercee.domain.groups.Group;
 import com.kodilla.ecommercee.domain.order.OrderEntity;
 import com.kodilla.ecommercee.domain.user.UserEntity;
 import org.junit.Assert;
@@ -15,30 +10,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserDaoTestSuite {
     @Autowired
     private UserEntityDao userDao;
-    @Autowired
-    private ProductDao productDao;
-    @Autowired
-    private CartEntityDao cartEntityDao;
     private static final String NAME = "John";
     private static final String SURNAME = "Rambo";
     private static final String LOGIN = "Test_Login";
     private static final String MAIL = "mail@test.com";
     private static final String PASSWORD = "testpassword";
     private static final Integer SESSION_KEY = 12345;
-
-    @Test
-    public void contextLoads(){}
 
     @Test
     public void testUserDaoSave(){
@@ -53,11 +38,10 @@ public class UserDaoTestSuite {
         Long id = user1.getId();
         Optional<UserEntity> readUser = userDao.findById(id);
         Assert.assertTrue(readUser.isPresent());
-        System.out.println(readUser.get().getId() + " | " + readUser.get().getName() + " |" + readUser.get().getSurname());
 
 
         //CleanUp
-        userDao.deleteById(id);
+        //userDao.deleteById(id);
     }
 
     @Test
@@ -95,17 +79,13 @@ public class UserDaoTestSuite {
         Assert.assertEquals(readOrder2Adress, "Adress 2");
         Assert.assertEquals(BigDecimal.valueOf(30.99), readCart);
         Assert.assertEquals(readCartUser, "Test Login 2");
-        System.out.println(
-                readUser.get().getId() + " | " + readUser.get().getName()
-                        + " |" + readUser.get().getSurname()
-                        + " |" + readUser.get().getCart().getUser().getName()
-                        + " |" + readUser.get().getCart().getCost().toString()
-        );
-        readUser.get().getOrders().stream().map(OrderEntity::getAdress)
-                .forEach(s -> System.out.println("User orders adress : " + s));
-        readUser.get().getOrders().stream().map(OrderEntity::getUser).map(UserEntity::getId)
-                .forEach(s -> System.out.println("Order user Id : " + s));
-        System.out.println("Cart user Id : " + readUser.get().getCart().getUser().getId());
+        Assert.assertEquals(readUser.get().getId().longValue(), 1L);
+        Assert.assertEquals(readUser.get().getName(), "John");
+        Assert.assertEquals(readUser.get().getSurname(), "Rambo");
+        Assert.assertEquals(readUser.get().getOrders().get(0).getUser().getId().longValue(), 1L);
+        Assert.assertEquals(readUser.get().getOrders().get(1).getUser().getId().longValue(), 1L);
+        Assert.assertEquals(readUser.get().getCart().getUser().getId().longValue(), 1L);
+
 
 
 
