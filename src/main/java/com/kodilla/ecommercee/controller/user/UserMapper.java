fa84e13cbfa3922ab371cd460f13c943cc626fc8
naros1 <sorankasim@gmail.com>
2020-01-08 22:14:40 +0100

    Dodałem jeszcze mapper

---
+++ src/main/java/com/kodilla/ecommercee/controller/user/UserMapper.java
@@ -0,0 +1,29 @@
+package com.kodilla.ecommercee.controller.user;
+
+import com.kodilla.ecommercee.domain.user.UserEntity;
+import com.kodilla.ecommercee.domain.user.UserEntityDto;
+
+public class UserMapper {
+    public UserEntity mapToUserEntity(final UserEntityDto userEntityDto){
+        return new UserEntity(
+                userEntityDto.getName(),
+                userEntityDto.getSurname(),
+                userEntityDto.getLogin(),
+                userEntityDto.getMail(),
+                userEntityDto.getPassword()
+        );
+    }
+
+    public UserEntityDto mapToUserEntityDto(final UserEntity userEntity){
+        return new UserEntityDto(
+                userEntity.getId(),
+                userEntity.getName(),
+                userEntity.getSurname(),
+                userEntity.getLogin(),
+                userEntity.getMail(),
+                userEntity.getPassword(),
+                userEntity.getSessionKey(),
+                userEntity.getLoggedIn()
+        );
+    }
+}
