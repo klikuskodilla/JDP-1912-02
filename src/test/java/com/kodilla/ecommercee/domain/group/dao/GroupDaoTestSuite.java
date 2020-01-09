74da464f3ff25d4e2b2256cd7685963cb7881a4b
DELL <marcin.szymanski.poczta@gmail.com>
2020-01-09 12:54:15 +0100

    assertion tests update - rebuild testFindProductInGroup

--- src/test/java/com/kodilla/ecommercee/domain/group/dao/GroupDaoTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/group/dao/GroupDaoTestSuite.java
@@ -10,14 +10,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
-import java.util.List;
 import java.util.Optional;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -26,6 +22,8 @@ public class GroupDaoTestSuite {
     private Group group2;
     private Product product1;
     private Product product2;
+    private Product product3;
+    private Product product4;
 
     @Autowired
     private GroupDao groupDao;
@@ -36,68 +34,47 @@ public class GroupDaoTestSuite {
     public void sampleData(){
         product1 = new Product("RED",new BigDecimal(2.22));
         product2 = new Product("CAT", new BigDecimal(1.22));
+        product3 = new Product("BLUE", new BigDecimal(3.22));
+        product4 = new Product("DOG",new BigDecimal(0.43));
 
         group1 = new Group("Group Of Colors");
         group2 = new Group("Group Of Animals");
     }
 
 
-    @Test
-    public void testCreateGroup(){
 
-        product1.setGroup(group1);
-        group1.getProductsGroup().add(product1);
-        groupDao.save(group1);
-
-        product2.setGroup(group2);
-        group2.getProductsGroup().add(product2);
-        groupDao.save(group2);
-
-        Long groupId = group1.getGroupId();
-
-        Optional<Group> isReal = groupDao.findById(groupId);
-        assertTrue(isReal.isPresent());
-        assertEquals("Group Of Colors",isReal.get().getGroupName());
-
-        //CleanUp
-        groupDao.deleteAll();
-    }
-
-    @Test
-    public void findGroups(){
-        product1.setGroup(group1);
-        group1.getProductsGroup().add(product1);
-        groupDao.save(group1);
-
-        product2.setGroup(group2);
-        group2.getProductsGroup().add(product2);
-        groupDao.save(group2);
-
-        List<Group> isFind = groupDao.findAll();
-
-        assertEquals(2, isFind.size());
-
-        //CleanUp
-        groupDao.deleteAll();
-    }
     @Test
     public void testFindProductInGroup(){
+
+        //Given
         product1.setGroup(group1);
         group1.getProductsGroup().add(product1);
+        product3.setGroup(group1);
+        group1.getProductsGroup().add(product3);
+
         groupDao.save(group1);
 
         product2.setGroup(group2);
         group2.getProductsGroup().add(product2);
+        product4.setGroup(group2);
+        group2.getProductsGroup().add(product4);
+
         groupDao.save(group2);
 
+        //When
         Long groupId = group2.getGroupId();
         Long productId = product2.getId();
 
-        Optional<Group> isReal = groupDao.findById(groupId);
-        String isProduct = productDao.findById(productId).get().getDescription();
+        Optional<Group> findGroupById = groupDao.findById(groupId);
+        String product3Description = product3.getDescription();
 
-        assertTrue(isReal.isPresent());
-        assertEquals("Group Of Animals",isReal.get().getGroupName());
-        assertEquals(isProduct,productDao.findById(productId).get().getDescription());
+        //Then
+        assertTrue(findGroupById.isPresent());
+        assertEquals(4,productDao.count());
+        assertEquals("Group Of Animals",findGroupById.get().getGroupName());
+        assertEquals("BLUE",product3Description);
+        assertEquals("CAT",productDao.findById(productId).get().getDescription());
+        assertEquals(findGroupById.get().getGroupId(),product2.getGroup().getGroupId());
+        assertNotEquals(group1.getGroupId(),product4.getGroup().getGroupId());
     }
 }
