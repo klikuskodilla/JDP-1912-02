5943f6b4a0946b3543254498f324e52b8d6cfdfb
Marcin Szymanski <marcin.szymanski.poczta@gmail.com>
2020-01-12 20:35:19 +0100

    Update ProductDto.java

--- src/main/java/com/kodilla/ecommercee/domain/Product/ProductDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product/ProductDto.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class ProductDto {
 
     private Long id;
+    private String name;
     private String description;
     private BigDecimal price;
     private Group group;
