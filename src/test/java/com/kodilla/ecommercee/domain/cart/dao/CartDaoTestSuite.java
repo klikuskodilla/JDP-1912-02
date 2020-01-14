b3eec9094aee4e068c7c42e205e28d9077138ade
KrzysztofZakrocki <zakrocki.krzysztof@gmail.com>
2020-01-14 21:54:41 +0100

    fix - resolved conflict in cartDaoTestSuite

--- src/test/java/com/kodilla/ecommercee/domain/cart/dao/CartDaoTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/domain/cart/dao/CartDaoTestSuite.java
@@ -11,8 +11,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.util.Optional;
 
