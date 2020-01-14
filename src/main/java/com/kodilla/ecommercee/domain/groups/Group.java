b34cac04a69f95e013a014d8e80d5c0a06ec5cc9
naros1 <sorankasim@gmail.com>
2020-01-14 19:13:33 +0100

    update Group i GroupDao z branchami z Gita

--- src/main/java/com/kodilla/ecommercee/domain/groups/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/groups/Group.java
@@ -13,42 +13,22 @@ import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
-@Table(name = "GROUP_PRODUCT")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity(name = "GROUP_PRODUCT")
 public class Group {
-
-    private Long group_id;
-    private String groupName;
-    private List<Product> productsGroup;
-
-    public Group(String groupName) {
-        this.groupName = groupName;
-        this.productsGroup = new ArrayList<>();
-    }
-
-    public Group() {}
 
     @Id
     @NotNull
     @GeneratedValue
-    @Column(name = "GROUP_ID")
-    public Long getGroupId() {
-        return group_id;
-    }
-
-    public void setGroupId(Long groupId) {
-        this.group_id = groupId;
-    }
+    @Column(name = "GROUP_ID", unique = true)
+    private Long groupId;
 
     @NotNull
     @Column(name = "GROUP_NAME")
-    public String getGroupName() {
-        return groupName;
-    }
-
-    public void setGroupName(String groupName) {
-        this.groupName = groupName;
-    }
+    private String groupName;
 
     @OneToMany(
             targetEntity = Product.class,
@@ -56,11 +36,12 @@ public class Group {
             cascade = CascadeType.ALL,
             fetch = FetchType.LAZY
     )
-    public List<Product> getProductsGroup() {
-        return productsGroup;
+    private List<Product> products;
+
+    public Group(String groupName) {
+        this.groupName = groupName;
+        this.products = new ArrayList<>();
     }
 
-    public void setProductsGroup(List<Product> productsGroup) {
-        this.productsGroup = productsGroup;
-    }
+
 }
--- src/main/java/com/kodilla/ecommercee/domain/groups/dao/GroupDao.java
+++ src/main/java/com/kodilla/ecommercee/domain/groups/dao/GroupDao.java
@@ -5,7 +5,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Transactional
 @Repository
 public interface GroupDao extends CrudRepository<Group, Long> {
+
+    Optional<Group> findByGroupName(String groupName);
 }
