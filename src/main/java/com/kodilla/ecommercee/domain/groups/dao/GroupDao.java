242442f673433be0f17b7628896644f0769150fa
naros1 <sorankasim@gmail.com>
2020-01-16 21:39:59 +0100

    try 1 fix branch JDP191202 - 21

--- src/main/java/com/kodilla/ecommercee/domain/groups/dao/GroupDao.java
+++ src/main/java/com/kodilla/ecommercee/domain/groups/dao/GroupDao.java
@@ -3,26 +3,13 @@ package com.kodilla.ecommercee.domain.groups.dao;
 import com.kodilla.ecommercee.domain.groups.Group;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import javax.transaction.Transactional;
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.Optional;
 
-@Repository
 @Transactional
+@Repository
 public interface GroupDao extends CrudRepository<Group, Long> {
 
-    @Override
-    List<Group> findAll();
-
-    @Override
-    Optional<Group> findById(Long group_id);
-
-    @Override
-    Group save(Group group);
-
-    @Override
-    void deleteById(Long group_id);
-
-    @Override
-    long count();
+    Optional<Group> findByGroupName(String groupName);
 }
