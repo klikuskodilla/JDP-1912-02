8e03ba09835514d866828a7ea90e1957a99cbd9a
KrzysztofZakrocki <zakrocki.krzysztof@gmail.com>
2020-01-19 21:15:18 +0100

    repair return orderDto in orderController

--- src/main/java/com/kodilla/ecommercee/controller/order/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/order/OrderController.java
@@ -5,6 +5,7 @@ import com.kodilla.ecommercee.controller.order.OrderNotFoundException;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -16,13 +17,13 @@ public class OrderController {
     @RequestMapping(method = RequestMethod.GET, value = "getOrderList")
     public List<OrderDto> getOrdersList() {
         List<OrderDto> testOrderList = new ArrayList<>();
-        testOrderList.add(new OrderDto(1L, "test adress", false, new Date()));
+        testOrderList.add(new OrderDto(1L, "test adress", false, new Date(), 1L, 1L, new BigDecimal(321)));
         return testOrderList;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getOrder")
     public OrderDto getOrder(@RequestParam Long orderId) throws OrderNotFoundException {
-        return new OrderDto(2L, "testAddress2", true, new Date());
+        return new OrderDto(2L, "testAddress2", true, new Date(), 2L, 3L, new BigDecimal(2341));
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createNewOrder", consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -32,7 +33,7 @@ public class OrderController {
 
     @RequestMapping(method = RequestMethod.PUT, value = "updateOrder", consumes = MediaType.APPLICATION_JSON_VALUE)
     public OrderDto updateOrder(@RequestBody OrderDto orderDto) {
-        return new OrderDto(3L, "testAddress3", false, new Date());
+        return new OrderDto(3L, "testAddress3", false, new Date(), 3L, 3L, new BigDecimal(321));
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteOrder")
